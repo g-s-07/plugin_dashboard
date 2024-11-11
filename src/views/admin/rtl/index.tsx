a32c5f6e2d93b625e3b4644d5414b30b39c65c70
Gaurav Sharma <gaurav.sharma@infoanalytica.com>
2024-11-11 15:56:04 +0530

    fix: imports

--- src/views/admin/rtl/index.tsx
+++ src/views/admin/rtl/index.tsx
@@ -34,7 +34,6 @@ import {
 // Assets
 import Usa from 'img/dashboards/usa.png';
 // Custom components
-import MiniCalendar from 'components/calendar/MiniCalendar';
 import MiniStatistics from 'components/card/MiniStatistics';
 import IconBox from 'components/icons/IconBox';
 import React from 'react';
@@ -159,7 +158,6 @@ export default function UserReports() {
         <ComplexTable tableData={tableDataComplex} />
         <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap="20px">
           <Tasks />
-          <MiniCalendar h="100%" minW="100%" selectRange={false} />
         </SimpleGrid>
       </SimpleGrid>
     </Box>
