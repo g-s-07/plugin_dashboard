cc54aa2e4f330de933c550973d4c49ebe27035d8
Gaurav Sharma <gaurav.sharma@infoanalytica.com>
2024-11-25 19:17:08 +0530

    add: improved UI

--- src/components/card/GeneralCard.tsx
+++ src/components/card/GeneralCard.tsx
@@ -23,212 +23,16 @@ import {
   Button,
   useToast
 } from '@chakra-ui/react';
-import { useQuery } from '@tanstack/react-query';
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 // Custom components
 import Card from 'components/card/Card';
+import { FaClock } from "react-icons/fa";
 import { showTable } from 'components/tables/SimpleTable';
 import { FaInfoCircle } from 'react-icons/fa';
-import { PiArrowFatLinesUpFill, PiArrowFatLinesDownFill } from "react-icons/pi";
+import { FaHourglassStart } from "react-icons/fa";
 import { useState } from 'react';
 import { BACKEND_DOMAIN, token } from '../../../urls';
-
-
-// export default function GeneralCard(props:{
-//     info: any
-//     height?: string
-//     overflowY?: string
-//     description?: string
-//     icon?: JSX.Element
-//     type: string
-// }){
-//     const {info, overflowY, description, icon, type} = props;
-//     const textColor = useColorModeValue('navy.700', 'white');
-//     const toast = useToast();
-
-//     const refreshData = (table_name: string) => {
-//       try {
-//         const {
-//           data: playrightList,
-//           error,
-//           isLoading,
-//           isFetching,
-//           isSuccess,
-//         }: {
-//             data: { data: TableCountByName };
-//             error: AxiosError;
-//             isLoading: boolean;
-//             isSuccess: boolean;
-//             isFetching: boolean;
-//         } = useQuery({
-//             queryKey: [`data${type.charAt(0).toLocaleUpperCase() + type.toString().slice(1)}ByTableName`],
-//             queryFn: () => getTableCountsByName(type, table_name),
-//         });
-
-//         const data = playrightList?.data;
-
-//         if (isSuccess) {
-//           toast.closeAll();
-//           toast({
-//             title: "Success",
-//             description: `${table_name} fetched successfully`,
-//             status: "success",
-//             duration: 5000,
-//             isClosable: true,
-//           });
-//         }
-
-//       }
-//       catch (error:any) {
-//         toast.closeAll();
-//         toast({
-//           title: 'Error',
-//           description: error?.message,
-//           status: 'error',
-//           duration: 5000,
-//           isClosable: true,
-//         })
-//       }
-
-//     };
-
-
-
-//     return (
-//       <Card
-//         width={'full'}
-//         bg={'#ffffff1a'}
-//         brightness={0.2}
-//         mt={4}
-//         borderRadius={'2px'}
-//         height={props.height}
-//         overflowY={overflowY==='scroll' ? 'scroll' : 'hidden'}
-//       >
-//         <Stack>
-//             <Accordion
-//                 width={'full'}
-//                 allowToggle
-//                 borderColor={'transparent'}
-//             >
-//                 <Flex 
-//                     justify={'space-between'}
-//                     align={'center'}
-//                     p={2}
-//                 >
-//                     <Text fontSize={'md'} fontWeight={'bold'}>
-//                         {description}
-//                     </Text>
-//                     <Text fontSize={'md'} fontWeight={'bold'}>
-//                         Total Count: {info?.length}
-//                     </Text>
-//                 </Flex>
-//                 <AccordionItem>
-//                     <SimpleGrid
-//                         mt={4}
-//                         columns={{
-//                         sm: 1,
-//                         md: 2,
-//                         lg: 3,
-//                         xl: 4,
-//                         }}
-//                         gap={4}
-//                     >
-//                        {info?.map((item: { table_name: string, row_count: number, columns_list: [any], last_present_time: any }, index: number) => (
-//                             <Card p="20px" key={index}>
-//                                 <Flex direction="column" justify="center" align="center" h="100%">
-//                                     <Flex flexDirection="column" justify="center" align="center" h="100%">
-//                                         <Flex
-//                                             justify="center"
-//                                             align="center"
-//                                             direction={{
-//                                                 base: 'row',
-//                                                 md: 'column',
-//                                                 lg: 'row',
-//                                                 xl: 'column',
-//                                                 '2xl': 'row',
-//                                             }}
-//                                             mb="auto"
-//                                             w="100%"
-//                                         >
-//                                             <Flex direction="column" align="center" justify="center">
-//                                               <Flex direction="row" align="center" justify="center" gap={2}>
-//                                                   <Text
-//                                                       color={textColor}
-//                                                       fontSize={{
-//                                                           base: 'sm',
-//                                                           md: 'md',
-//                                                           lg: 'lg',
-//                                                           xl: 'lg',
-//                                                           '2xl': 'md',
-//                                                           '3xl': 'lg',
-//                                                       }}
-//                                                       mb="5px"
-//                                                       fontWeight="bold"
-//                                                       textAlign="center"
-//                                                   >
-//                                                       {item.table_name}
-//                                                   </Text>
-//                                                   <Box position="relative" pb={2}>
-//                                                       <Tooltip 
-//                                                           hasArrow
-//                                                           aria-label="last modified"
-//                                                           label={item.last_present_time ? `Last Modified: ${item.last_present_time}` : "null"}
-//                                                           placement="right"
-//                                                           fontSize="xl"
-//                                                           pt={2}
-//                                                       >
-//                                                           <Box as="span">
-//                                                             <Button
-//                                                               size={'sm'}
-//                                                               onClick={() => refreshData(item.table_name)}
-//                                                             >
-//                                                               {icon}
-//                                                             </Button>
-//                                                           </Box>
-//                                                       </Tooltip>
-//                                                   </Box>
-//                                               </Flex>
-
-//                                                 <Tooltip 
-//                                                     hasArrow
-//                                                     aria-label='A tooltip'
-//                                                     label={showTable(item.columns_list)}
-//                                                     placement='right'
-//                                                     fontSize="xl"
-//                                                     pt={2}
-//                                                 >
-//                                                     <Box position="absolute" top={1} right={1} p={1} fontSize={"xl"}>
-//                                                         <FaInfoCircle />
-//                                                     </Box>
-//                                                 </Tooltip>
-//                                                 <Text
-//                                                     color={textColor}
-//                                                     fontSize={{
-//                                                         base: 'xl',
-//                                                         md: 'xl',
-//                                                         lg: 'xl',
-//                                                         xl: 'xl',
-//                                                         '2xl': 'xl',
-//                                                         '3xl': 'xl',
-//                                                     }}
-//                                                     fontWeight="bold"
-//                                                     textAlign="center"
-//                                                 >
-//                                                     {item.row_count}
-//                                                 </Text>
-//                                             </Flex>
-//                                         </Flex>
-//                                     </Flex>
-//                                 </Flex>
-//                             </Card>
-//                         ))}
-//                     </SimpleGrid>
-//                 </AccordionItem>
-//             </Accordion>
-//         </Stack>
-//       </Card>
-//     );
-// }
+import {getDifference, getCurrentDateTime} from 'utils/dateDifference';
 
 
 export default function GeneralCard(props: {
@@ -240,11 +44,18 @@ export default function GeneralCard(props: {
   type: string;
 }) {
   const { info, overflowY, description, icon, type } = props;
+  console.log(info);
   const textColor = useColorModeValue('navy.700', 'white');
+  const colors = [
+    useColorModeValue('red.400', 'red.500'),
+    useColorModeValue('gray.300', 'gray.600'),
+    useColorModeValue('yellow.300', 'yellow.500'),
+    useColorModeValue('green.300', 'green.500'),
+    useColorModeValue('purple.300', 'purple.600'),
+  ]
   const toast = useToast();
   const [localInfo, setLocalInfo] = useState(props.info);
-
-
+  
   const handleRefresh = async (table_name: string, index: number) => {
     try {
       const response = await axios.get(
@@ -287,7 +98,27 @@ export default function GeneralCard(props: {
     }
   }
 
-
+  
+  const calculateCounts = () => {
+    const counts = [0, 0, 0, 0]; 
+    localInfo.forEach((item: { last_present_time: any }) => {
+      const diff = getDifference(getCurrentDateTime(), item.last_present_time);
+  
+      if (diff <= 1440) {
+        counts[0] += 1;
+      } else if (diff > 1440 && diff <= 4320) {
+        counts[1] += 1;
+      } else if (diff > 4320 && diff <= 10080) {
+        counts[2] += 1;
+      } else {
+        counts[3] += 1;
+      }
+    });
+    return counts;
+  };
+  
+  const counts = calculateCounts();
+  
 
   return (
     <Card
@@ -304,18 +135,40 @@ export default function GeneralCard(props: {
           width={'full'}
           allowToggle
           borderColor={'transparent'}
-        >
+          >
+            <Flex justifyContent={'end'}>
+              <Text fontSize={'md'} fontWeight={'bold'}>
+                <Flex align="center" gap={2}>
+                  <Box bg={colors[3]} width="15px" height="15px" borderRadius="full" /> &lt;= 24 hours
+                  <Box bg={colors[2]} width="15px" height="15px" borderRadius="full" /> &gt;24 hours and  &lt;= 72 hours
+                  <Box bg={colors[0]} width="15px" height="15px" borderRadius="full" /> &gt;72 hours and &lt;7 days
+                  <Box bg={colors[4]} width="15px" height="15px" borderRadius="full" /> &gt;7 days
+                </Flex>
+              </Text>
+            </Flex>
           <Flex
             justify={'space-between'}
             align={'center'}
-            p={2}
+            p={4}
+            border={'1px solid'}
+            borderColor={'white.400'}
+            borderRadius={'10px'}
+            mt={3}
           >
             <Text fontSize={'md'} fontWeight={'bold'}>
               {description}
             </Text>
-            <Text fontSize={'md'} fontWeight={'bold'}>Total Count: {info?.length}
-              <strong>Total Count: {info?.length}</strong>
-            </Text>
+            <Flex 
+              direction={'row'}
+              gap={2}
+              align={"center"}
+            >
+                <Text fontSize={"25"} fontWeight={"extrabold"} color={colors[3]}>{counts[0]}</Text> <Box fontWeight={"extrabold"} fontSize={"25"}>+</Box> 
+                <Text fontSize={"25"} fontWeight={"extrabold"} color={colors[2]}>{counts[1]}</Text> <Box fontWeight={"extrabold"} fontSize={"25"}>+</Box> 
+                <Text fontSize={"25"} fontWeight={"extrabold"} color={colors[0]}>{counts[2]}</Text> <Box fontWeight={"extrabold"} fontSize={"25"}>+</Box> 
+                <Text fontSize={"25"} fontWeight={"extrabold"} color={colors[4]}>{counts[3]}</Text> <Box fontWeight={"extrabold"} fontSize={"25"}>=</Box> 
+                <Text fontSize={"25"} fontWeight={"extrabold"}>{info?.length}</Text> 
+            </Flex>
           </Flex>
           <AccordionItem>
             <SimpleGrid
@@ -380,7 +233,7 @@ export default function GeneralCard(props: {
                               <Tooltip
                                 hasArrow
                                 aria-label="last modified"
-                                label={item.last_present_time ? `Last Modified: ${item.last_present_time}` : "null"}
+                                label={item.last_present_time ? `Last Modified: ${new Date(new Date(`${item.last_present_time}`).toLocaleString("en-US", { timeZone: "Asia/Kolkata" })).toString()}` : "null"}
                                 placement="right"
                                 fontSize="xl"
                                 pt={2}
@@ -414,19 +267,37 @@ export default function GeneralCard(props: {
                             <Text
                               color={textColor}
                               fontSize={{
-                                base: 'xl',
-                                md: 'xl',
-                                lg: 'xl',
-                                xl: 'xl',
-                                '2xl': 'xl',
-                                '3xl': 'xl',
+                                base: '2xl',
+                                md: '2xl',
+                                lg: '2xl',
+                                xl: '2xl',
+                                '2xl': '2xl',
+                                '3xl': '2xl',
                               }}
                               fontWeight="bold"
                               textAlign="center"
                             >
-                              {item.row_count}
+                              {
+                                getDifference(getCurrentDateTime(), item.last_present_time) <= 60 ? (
+                                  <Flex
+                                    direction={"row"}
+                                    align="center"
+                                    justify="center"
+                                    gap={1}
+                                  >
+                                    <FaHourglassStart color={"#FF80AB"}/> <Text color={colors[3]}>{item.row_count}</Text>
+                                  </Flex>
+                                ) : getDifference(getCurrentDateTime(), item.last_present_time) > 60 && getDifference(getCurrentDateTime(), item.last_present_time) <= 1440 ? (
+                                  <Text color={colors[3]}>{item.row_count}</Text>
+                                ) : getDifference(getCurrentDateTime(), item.last_present_time) > 1440 && getDifference(getCurrentDateTime(), item.last_present_time) <= 4320 ? (
+                                  <Text color={colors[2]}>{item.row_count}</Text>
+                                ) : getDifference(getCurrentDateTime(), item.last_present_time) > 4320 && getDifference(getCurrentDateTime(), item.last_present_time) <= 10080 ? (
+                                  <Text color={colors[0]}>{item.row_count}</Text>
+                                ) : (
+                                  <Text color={colors[4]}>{item.row_count}</Text>
+                                )
+                              }
                             </Text>
-                            <PiArrowFatLinesUpFill color={"01B574"} size={25} />
                           </Flex>
                         </Flex>
                       </Flex>
